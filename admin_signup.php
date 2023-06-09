aa0f63ac690b9156fb14f3b5e4c96d35edc74474
elias-avenger <eli0@outlook.com>
2023-06-09 13:00:24 +0300

    Modified admin_signup form to suit we want

--- admin_signup.php
+++ admin_signup.php
@@ -1,5 +1,9 @@
 <?php
-    echo "Admin Signup";
+    include "queries.php";
+    $users = getData('users');
+    if(!empty($users)){
+        header("location:index.php");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,34 +15,43 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-
     <div class="min-h-screen flex items-center justify-center bg-gray-100">
         <div class="max-w-md w-full mx-auto p-4">
-          <h2 class="text-3xl font-bold text-center mb-4">Client Signup</h2>
+          <h2 class="text-3xl font-bold text-center mb-4">Admin (Creator) Signup</h2>
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div class="mb-4">
-              <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name:</label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Enter your name">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="name">First Name:</label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="fname" type="text" placeholder="Enter your first name">
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Last Name:</label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="lname" type="text" placeholder="Enter your last name">
             </div>
             <div class="mb-4">
               <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email:</label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter your email">
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" placeholder="Enter your email">
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Phone:</label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="phone" type="tel" placeholder="Enter your phone number">
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Date of Birth:</label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="dob" type="date" placeholder="Enter your Date of Birth">
             </div>
             <div class="mb-6">
               <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password:</label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password">
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" placeholder="Enter your password">
+            </div>
+            <div class="mb-6">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Confirm Password:</label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="comf_password" type="password" placeholder="Re-enter your password">
             </div>
             <div class="flex items-center justify-between">
               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign Up</button>
             </div>
           </form>
-          <p class="text-center text-gray-500 text-xs">
-            Already have an account? 
-            <a class="text-blue-500 hover:text-blue-800" href="#">Log in</a>
-          </p>
         </div>
-      </div>
-      
-  
+    </div>
 </body>
 </html>
