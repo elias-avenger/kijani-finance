3a674fd63913b9c6665a92b42053486eec87a2fc
elias-avenger <eli0@outlook.com>
2023-06-11 15:37:03 +0300

    Changed title to Kijani Forestry

--- admin_signup.php
+++ admin_signup.php
@@ -11,7 +11,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Kijani Forestry</title>
   <link rel="icon" type="image/x-icon" href="images/KF_Green_Icon.png">
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
