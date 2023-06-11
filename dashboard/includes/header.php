21eb014d8a621790a450b83ab018bb7783334adc
elias-avenger <eli0@outlook.com>
2023-06-11 15:33:46 +0300

    added a favicon

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -4,9 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dashboard</title>
+  <title>Kijani Forestry</title>
+  <link rel="icon" type="image/x-icon" href="../images/KF_Green_Icon.png">
   <!-- Tailwind CSS CDN -->
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
+  
 </head>
 
 <body>
