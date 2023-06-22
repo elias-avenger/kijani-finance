65a1577bb3a62632e4c06a400c3c2e8307a5e634
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-06-22 13:49:48 +0300

    modified the logout link

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -32,7 +32,6 @@
           <li><a href="dashboard.php" class="hover:text-gray-300">Home</a></li>
           <li><a href="dash_budget.php" class="hover:text-gray-300">Budgets</a></li>
           <li><a href="dash_category.php" class="hover:text-gray-300">Category</a></li>
-          <li><a href="#" class="hover:text-gray-300">Profile</a></li>
           <li><a href="dash_users.php" class="hover:text-gray-300">Users</a></li>
           <li>
             <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white rounded md:border-0 md:p-0 md:w-auto"><?php echo $user_data['fname']." ".$user_data['lname'];?><svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
@@ -47,7 +46,7 @@
                   </li>
                 </ul>
                 <div class="py-1">
-                  <a href="#" class="block px-4 py-2 text-sm text-red-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-red-400 dark:hover:text-white">Logout</a>
+                  <a href="../controllers/logout.php" class="block px-4 py-2 text-sm text-red-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-red-400 dark:hover:text-white">Logout</a>
                 </div>
             </div>
           </li>
