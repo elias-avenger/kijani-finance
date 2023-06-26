6a5ede64388e0e565c504e0b4fa8253f8e21f700
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-06-26 12:22:47 +0300

    modified the form inputs

--- dashboard/dash_users.php
+++ dashboard/dash_users.php
@@ -56,7 +56,7 @@
                                 </button>
                                 <div class="p-6 text-center">
                                     <svg aria-hidden="true" class="mx-auto mb-4 text-white w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                    <h3 class="mb-5 text-lg font-normal text-white dark:text-gray-400">Are you sure you want to delete this user?</h3>
+                                    <h3 class="mb-5 text-lg font-normal text-white ">Are you sure you want to delete this user?</h3>
                                     <form action="../controllers/delete.php" method="POST" class="inline-flex">
                                       <input type="hidden" value="<?php echo $user['id'];?>" name="id">
                                       <button name="delete_user" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
@@ -132,6 +132,7 @@
       </div>
 
       <div class="relative bg-white shadow-md rounded-md p-4">
+        <!-- Addd new user button -->
         <button data-modal-toggle="add-user-form" class="bg-green-800 text-white mb-2 px-1 py-2 rounded-md hover:bg-green-600 w-full">
           Add new user
         </button>
@@ -147,7 +148,7 @@
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               </button>
               <div class="px-6 py-6 lg:px-8">
-                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Enter new user details below</h3>
+                <h3 class="mb-4 text-xl font-bold text-green-800 dark:text-white">Enter new user details below</h3>
 
                 <!-- Add User Form -->
                 <form  action="../controllers/submit.php" method="POST">
@@ -158,39 +159,39 @@
                     <div class="flex flex-col md:flex-row gap-2">
                       <div class="mt-4 md:w-1/2">
                         <label for="fname" class="block">First Name:</label>
-                        <input type="text" name="fname" id="fname" class="w-full md:w-48 border-2 border-gray-300 rounded-md p-1" required>
+                        <input type="text" name="fname" id="fname" class="w-full md:w-48 border-2 border-green-900 rounded-md p-1" required>
                       </div>
                       <div class="mt-4 md:w-1/2">
                         <label for="lname" class="block">Last Name:</label>
-                        <input type="text" name="lname" id="lname" class="w-full md:w-48 border-2 border-gray-300 rounded-md p-1" required>
+                        <input type="text" name="lname" id="lname" class="w-full md:w-48 border-2 border-green-900 rounded-md p-1" required>
                       </div>
                     </div>
                     <div class="mt-4">
                       <label for="email" class="block">Email:</label>
-                      <input type="email" name="email" id="email" class="w-full border-2 border-gray-300 rounded-md p-1" required>
+                      <input type="email" name="email" id="email" class="w-full border-2 border-green-900 rounded-md p-1" required>
                     </div>
                     <div class="flex flex-col md:flex-row gap-2">
                       <div class="mt-4">
                         <label for="password" class="block">Password:</label>
-                        <input type="password" name="password" id="password" class="w-full md:w-48 border-2 border-gray-300 rounded-md p-1" required>
+                        <input type="password" name="password" id="password" class="w-full md:w-48 border-2 border-green-900 rounded-md p-1" required>
                       </div>
                       <div class="mt-4">
                         <label class="block " for="conf_password">Confirm Password:</label>
-                        <input name="conf_password" type="password" placeholder="Re-enter your password" class="w-full md:w-48 border-2 border-gray-300 rounded-md p-1" required>
+                        <input name="conf_password" type="password" placeholder="Re-enter your password" class="w-full md:w-48 border-2 border-green-900 rounded-md p-1" required>
                       </div>
                     </div>
                     <div class="mt-4">
                       <label for="phone" class="block">Phone Number:</label>
-                      <input type="tel" name="phone" id="phone" class="w-full border-2 border-gray-300 rounded-md p-1" required>
+                      <input type="tel" name="phone" id="phone" class="w-full border-2 border-green-900 rounded-md p-1" required>
                     </div>
                     <div class="mt-4">
                       <label for="dob" class="block">Date of Birth:</label>
-                      <input type="date" name="dob" id="dob" class="w-full border-2 border-gray-300 rounded-md p-1" required>
+                      <input type="date" name="dob" id="dob" class="w-full border-2 border-green-900 rounded-md p-1" required>
                     </div>
                     <div class="mt-4">
                       <label for="role" class="block">Role:</label>
-                      <input type="radio" name="role" id="roleA" value="A" class="border-2 border-gray-300 rounded-md p-1" required> Admin
-                      <input type="radio" name="role" id="roleB" value="B" class="border-2 border-gray-300 rounded-md p-1" required> Budgeting
+                      <input type="radio" name="role" id="roleA" value="A" class="border-2 border-green-900 rounded-md p-1" required> Admin
+                      <input type="radio" name="role" id="roleB" value="B" class="border-2 border-green-900 rounded-md p-1" required> Budgeting
                     </div>
                     <div class="mt-4">
                       <input type="submit" class="w-full bg-green-900 text-white px-4 py-2 rounded-md hover:bg-green-600" name="add-user" value="Add User">
@@ -201,6 +202,7 @@
             </div>
           </div>
         </div>
+        <!-- add department button -->
         <button data-modal-toggle="departmentModal" class="bg-green-800 text-white mb-2 mt-2 px-1 py-2 rounded-md hover:bg-green-600 w-full">
           Add Budgeting Entity/Department
         </button>
@@ -223,15 +225,15 @@
                   ?>
                   <div class="mt-4">
                     <label for="e-name" class="block">Entity/Department Name:</label>
-                    <input type="text" name="e-name" id="e-name" class="w-full border-2 border-gray-300 rounded-md p-1" required>
+                    <input type="text" name="e-name" id="e-name" class="w-full border-2 border-green-900 rounded-md p-1" required>
                   </div>
                   <div class="mt-4">
                     <label for="e-description" class="block">Description:</label>
-                    <textarea name="e-description" id="e-description" cols="40" rows="5" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+                    <textarea name="e-description" id="e-description" cols="40" rows="5" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-green-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
                   </div>
                   <div class="mt-4">
                     <label for="incharge" class="block">User Incharge</label>
-                    <select name="incharge" id="incharge" class="block py-2.5 px-0 w-full text-sm text-gray-800 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer" required>
+                    <select name="incharge" id="incharge" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-green-900 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer" required>
                       <option value="">Select User</option>
                       <?php
                       foreach($users as $user){
