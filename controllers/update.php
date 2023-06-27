6bd5e12a3a0abe61063f45c276d61d896ce410ec
elias-avenger <eli0@outlook.com>
2023-06-27 21:35:49 +0300

    Added the last name to be automatically determined

--- controllers/update.php
+++ controllers/update.php
@@ -7,7 +7,10 @@
         $id = $_POST['uid'];
         $full_name = explode(" ", $_POST['full-name']);
         $fn = $full_name[0];
-        $ln = $full_name[1];
+        if($full_name[2] > "") 
+            $ln = $full_name[1]." ".$full_name[2];
+        else
+            $ln = $full_name[1];
         $el = $_POST['email'];
         $pn = $_POST['phone'];
         $tp = $_POST['type'];
