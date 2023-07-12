74f670dff36d960162af272659e7f76bf4604d3f
elias-avenger <eli0@outlook.com>
2023-07-13 00:06:11 +0300

    Wrote code to add budgets on dush_budgets on admin dashboard

--- dashboard/dash_budgets.php
+++ dashboard/dash_budgets.php
@@ -15,7 +15,8 @@
       <?php
         addBPeriodModel();
         $b_units = getData('budgeting_entities');
-        $b_dates = specialQuery("SELECT DISTINCT submitted_on FROM budgets ORDER BY submitted_on DESC");
+        //$b_nos = specialQuery("SELECT DISTINCT budget_no FROM budgets ORDER BY submitted_on DESC LIMIT 20");
+        $b_periods = specialQuery("SELECT * FROM budget_periods WHERE _from - CURRENT_DATE < 7 ORDER BY _from DESC LIMIT 5; ")
       ?>
     </div>
   <!-- Budget Control Panel -->
@@ -36,53 +37,59 @@
         </thead>
         <tbody>
           <?php
-            foreach($b_units as $b_unit){
-              $unit_id = $b_unit['id'];
-              $unit_name = $b_unit['name'];
-              
-              $budgets = specialQuery("SELECT * FROM budgets WHERE entity = '$unit_id'");
-              if(count($budgets) >= 1){
-                $cost = 0.0;
-                $status_array = [];
-                foreach($budgets as $budget){
-                  $s_by = $budget['submitted_by'];
-                  $array_s_by = mysqli_fetch_array(specialNoResult("SELECT fname, lname FROM users WHERE id = '$s_by'"));
-                  $name_s_by = $array_s_by['fname']." ".$array_s_by['lname'];
-                  // $entity = $budget['entity'];
-                  // $entity_name = specialNoResult("SELECT name FROM budgeting_entities WHERE id = '$entity'")->fetch_column();
-                  $submitted = date("d-m-Y", strtotime($budget['submitted_on']));
-                  $p = $budget['period'];
-                  $array_period = mysqli_fetch_array(specialNoResult("SELECT name, _from, _to FROM budget_periods WHERE id = '$p'"));
-                  $period_name = $array_period['name']." (".$array_period['_from']." to ".$array_period['_to'].")";
-                  $cost += $budget['cost'];
-                  array_push($status_array, $budget['status']);
+            foreach($b_periods as $b_period){
+              $p_id = $b_period['id'];
+              $period_name = $b_period['name']." ( from ".$b_period['_from']." to ".$b_period['_to'].")";
+              foreach($b_units as $b_unit){
+                $unit_id = $b_unit['id'];
+                $unit_name = $b_unit['name'];
+                $budgets = specialQuery("SELECT * FROM budgets WHERE entity = '$unit_id' AND period = '$p_id'");
+                if(count($budgets) >= 1){
+                  $cost = 0.0;
+                  $status_array = [];
+                  foreach($budgets as $budget){
+                    $s_by = $budget['submitted_by'];
+                    $array_s_by = mysqli_fetch_array(specialNoResult("SELECT fname, lname FROM users WHERE id = '$s_by'"));
+                    $name_s_by = $array_s_by['fname']." ".$array_s_by['lname'];
+                    // $entity = $budget['entity'];
+                    // $entity_name = specialNoResult("SELECT name FROM budgeting_entities WHERE id = '$entity'")->fetch_column();
+                    $submitted = date("d-m-Y", strtotime($budget['submitted_on']));
+                    // $p = $budget['period'];
+                    // $array_period = mysqli_fetch_array(specialNoResult("SELECT name, _from, _to FROM budget_periods WHERE id = '$p'"));
+                    // $period_name = $array_period['name']." (".$array_period['_from']." to ".$array_period['_to'].")";
+                    $cost += $budget['cost'];
+                    array_push($status_array, $budget['status']);
+                  }
+                  if(in_array('N', $status_array) && !in_array('A', $status_array))
+                    $status = "Not Apporved";
+                  elseif(in_array('N', $status_array) && in_array('A', $status_array))
+                    $status = "In Progress";
+                  elseif(!in_array('N', $status_array) && in_array('A', $status_array))
+                    $status = "Approved";
+                  ?>
+                  <tr>
+                    <td class="p-2"><?php echo $name_s_by;?></td>
+                    <td class="p-2"><?php echo $unit_name;?></td>
+                    <td class="p-2"><?php echo $submitted;?></td>
+                    <td class="p-2"><?php echo $period_name;?></td>
+                    <td class="p-2"><?php echo $cost;?></td>
+                    <td class="p-2">
+                      <span class="bg-red-500 text-white text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300"><?php echo $status;?></span>
+                    </td>
+                    <td class="flex gap-4 p-2">
+                      <a href="#" data-tooltip-target="tooltip-bottom" data-tooltip-placement="bottom" type="button"><img src="visibility_black_24dp.svg" alt="view"></a>
+                      <div id="tooltip-bottom" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-green-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                        View budget
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                      </div>
+                    </td>
+                  </tr>
+                  <?php
                 }
-                if(in_array('N', $status_array) && !in_array('A', $status_array))
-                  $status = "Not Apporved";
-                elseif(in_array('N', $status_array) && in_array('A', $status_array))
-                  $status = "In Progress";
-                elseif(!in_array('N', $status_array) && in_array('A', $status_array))
-                  $status = "Approved";
-                ?>
-                <tr>
-                  <td class="p-2"><?php echo $name_s_by;?></td>
-                  <td class="p-2"><?php echo $unit_name;?></td>
-                  <td class="p-2"><?php echo $submitted;?></td>
-                  <td class="p-2"><?php echo $period_name;?></td>
-                  <td class="p-2"><?php echo $cost;?></td>
-                  <td class="p-2">
-                    <span class="bg-red-500 text-white text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300"><?php echo $status;?></span>
-                  </td>
-                  <td class="flex gap-4 p-2">
-                    <a href="#" data-tooltip-target="tooltip-bottom" data-tooltip-placement="bottom" type="button"><img src="visibility_black_24dp.svg" alt="view"></a>
-                    <div id="tooltip-bottom" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-green-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                      View budget
-                      <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
-                  </td>
-                </tr>
-                <?php
-              }
+                else{
+                  echo "<br>No budget submitted for ".$unit_name." for ".$period_name;
+                }
+              } 
             } 
           ?>
         </tbody>
