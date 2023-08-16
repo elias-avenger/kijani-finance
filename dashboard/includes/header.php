ba06a732be742018d52938a9888900c22b683e97
elias-avenger <eli0@outlook.com>
2023-08-16 21:47:13 +0300

    Updated the icons on the count boxes

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -10,8 +10,8 @@
     
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
+ <html lang="en">
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - kijani Forestry</title>
