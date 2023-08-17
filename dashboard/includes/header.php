430ce853211df2251e3cc84266cb9629ba47d84f
elias-avenger <eli0@outlook.com>
2023-08-17 13:23:29 +0300

    Updating the footer with the JS files to make the models among other parts work

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -27,8 +27,6 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.css"  rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js" integrity="sha256-xKeoJ50pzbUGkpQxDYHD7o7hxe0LaOGeguUidbq6vis=" crossorigin="anonymous"></script>
-        <script src="https://cdn.tailwindcss.com"></script>
-        
     </head>
 
     <body class="bg-gradient-to-b from-green-900 to-green-400 font-sans leading-normal tracking-normal mt-12">
